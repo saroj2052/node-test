e9f9d81e844e06f4337a9a4143ed18bcb815884a
saroj2052 <49181675+saroj2052@users.noreply.github.com>
2022-12-21 22:31:00 +0545

    Update 6080.js

--- 6080.js
+++ 6080.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function(req,res){
  res.writeHead(200, { 'Content-Type': 'text/plain' });
- res.end('Hello Teams Happy Dasai and tihar');
+ res.end('Hello Teams Happy Dasai and tihar $PORT');
 }).listen(8080);
 console.log('Server started on localhost:8080; press Ctrl-C to terminate...!');
