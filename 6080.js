112866f7982ae3052f72a358e4f892f00b3b0017
saroj shah <sarojshah2020@gmail.com>
2022-12-22 10:20:42 +0545

    updated 6080

--- 6080.js
+++ 6080.js
@@ -1,10 +1,7 @@
 
 var http = require('http');
-require('dotenv').config();
-process.env.TEST;
-const a = process.env.TEST;
 http.createServer(function(req,res){
  res.writeHead(200, { 'Content-Type': 'text/plain' });
  res.end('Hello Teams Happy Dasai and tihar');
-}).listen(a);
-console.log('Server started on localhost:8080; press Ctrl-C to terminate...!',process.env.TEST);
+}).listen(PORT);
+console.log('Server started on localhost:8080; press Ctrl-C to terminate...!');
